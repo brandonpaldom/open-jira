7170753f0fefdd7a1765fc5a0874528aa3f6fd97
Brandon Palmeros <brandonpaldom@gmail.com>
2023-02-28 20:01:07 -0600

    Font added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,11 @@ import { EntriesProvider } from '@/context/entries'
 import { UIProvider } from '@/context/ui'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { darkTheme, lightTheme } from '@/themes'
-import '@/styles/globals.css'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
